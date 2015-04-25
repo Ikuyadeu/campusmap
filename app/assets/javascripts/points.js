d00ab886319410b42928751d1f94f3998a693621
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2015-04-25 14:23:49 +0900

    show map

--- app/assets/javascripts/points.js
+++ app/assets/javascripts/points.js
@@ -1,2 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+class User < ActiveRecord::Base
+  geocoded_by :address
+  after_validation :geocode
+end
--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,4 +1,11 @@
 class MapController < ApplicationController
   def index
+    @points = Point.all
+    @hash = Gmaps4rails.build_markers(@points) do |user, marker|
+      marker.lat user.latitude
+      marker.lng user.longitude
+      marker.infowindow user.description
+      marker.json({title: user.title})
+    end
   end
 end
